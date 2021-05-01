4c5fab6ef691e4d87420e27177b21408cde44726
jlautaha <jon.lautaha@gmail.com>
2021-05-01 10:18:32 +0000

    trying trigger once again

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def predict():
         if result == 1:
             output = "Get on this rocketship. THIS STARTUP IS GOING TO THE MOON!"
         else:
-            output = "It's unlikely this startup will succeed. BUYER BEWARE!"
+            output = "It's unlikely this startup will succeed!"
     return output #render_template("index.html", prediction_text = output)
 
 if __name__ == '__main__':
